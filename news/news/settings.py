93192f0c92b2207c4c0a8637e06dfd79b681db4a
javier Roel <javierroel@MacBook-Air-de-javier.local>
2023-02-02 09:43:41 +0100

    Version de entrega 1

--- news/news/settings.py
+++ news/news/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frikis',
+    'django_fastdev',
+    'crispy_forms',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
